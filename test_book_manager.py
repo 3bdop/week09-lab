41eaaf5bdff48d068726a426f6cac23c8211d26c
Abdulrahman <77641682+3bdop@users.noreply.github.com>
2024-02-28 11:34:28 +0300

    Update test_book_manager.py

--- test_book_manager.py
+++ test_book_manager.py
@@ -15,12 +15,12 @@ class TestBookManager(unittest.TestCase):
     def test_remove_book(self):
         self.manager.add_book(self.book2)
         self.manager.remove_book('The House of Mirth by Edith Wharton')
-        self.assertIsNone(self.book2)
-    
+        self.assertEqual(self.manager.list_books(), [])
+        
     def test_remove_none_existent_book(self):
         self.manager.add_book(self.book1)
         self.manager.remove_book('idk book')
-        self.assertEqual(self.manager.list_books(), [self.book2])
+        self.assertEqual(self.manager.list_books(), [self.book1])
     
 if __name__ == '__main__':
     unittest.main()
