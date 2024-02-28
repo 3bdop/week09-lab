2b10c7916c4219dafc7164509e599e3b5edd4394
Abdulrahman <77641682+3bdop@users.noreply.github.com>
2024-02-28 11:25:55 +0300

    Create test_book_manager.py

---
+++ test_book_manager.py
@@ -0,0 +1,26 @@
+import unittest
+from book_manager import Book, BookManager
+
+class TestBookManager(unittest.TestCase):
+    def setUp(self):
+        self.manager = BookManager()
+        self.book1 = {'title':'East of Eden by John Steinbeck', 'author':'Ali'}
+        self.book2 = {'title':'The House of Mirth by Edith Wharton', 'author':'Omar'}
+
+    def test_add_and_list(self):
+        self.manager.add_book(self.book1)
+        self.manager.add_book(self.book2)
+        self.assertEqual(self.manager.list_books(), [self.book1, self.book2])
+
+    def test_remove_book(self):
+        self.manager.add_book(self.book2)
+        self.manager.remove_book('The House of Mirth by Edith Wharton')
+        self.assertIsNone(self.book2)
+    
+    def test_remove_none_existent_book(self):
+        self.manager.add_book(self.book1)
+        self.manager.remove_book('idk book')
+        self.assertEqual(self.manager.list_books(), [self.book1])
+    
+if __name__ == '__main__':
+    unittest.main()
